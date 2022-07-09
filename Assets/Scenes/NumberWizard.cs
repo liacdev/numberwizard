6be77bc8428fb35d82700b1064854a95fa5e54fd
liacdev <jagoblacksky@gmail.com>
2022-07-08 20:44:06 -0500

    Scoping variables and such

--- Assets/Scenes/NumberWizard.cs
+++ Assets/Scenes/NumberWizard.cs
@@ -4,40 +4,43 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
         int max = 1000;
         int min = 1;
-      //TODO: int myNumber;
+        int guess = 500;
 
-        Debug.Log("Welcome to Number Wizard, yo");
+    void Start()
+    {
+        Debug.Log("Welcome to Number Wizard, yo!");
         Debug.Log("Pick a number, don't tell me what it is...");
         Debug.Log("The highest number you may choose is: " + max);
         Debug.Log("The lowest number you may choose is: " + min);
 
-       // Debug.Log("Tell me if your number is higher or lower than " + myNumber);
+        Debug.Log("Tell me if your number is higher or lower than mu number");
         Debug.Log("Push Up = Higher, Push Down = Lower, Push Return or Enter = Correct");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //If the player pushes a certain key, we want to display a certain message
         //Unity docs Player Input - KeyCode
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             Debug.Log("Up arrow key pressed");
+            min = guess;
+            Debug.Log(guess);
         }
 
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             Debug.Log("Down arrow key pressed");
+            max = guess;
+            Debug.Log(guess);
         }
 
         else if (Input.GetKeyDown(KeyCode.Return))
         {
             Debug.Log("Enter key pressed");
+            
+            Debug.Log(guess);
         }
     }
 }
